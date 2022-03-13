8922df44033e2ec8c09f121309181045a82bf9e6
Soroushes <99620495+Soroushes@users.noreply.github.com>
2022-03-13 15:52:42 +0330

    Add files via upload

---
+++ L3/homework1/login.java
@@ -0,0 +1,10 @@
+public class login {
+    public String username ;
+    public String name ;
+    public String pass ;
+    public login(String username , String name , String pass){
+        this.pass=pass ;
+        this.username=username;
+        this.name=name ;
+    }
+}
--- L3/homework1/main.java
+++ L3/homework1/main.java
@@ -1,1 +1,35 @@
+import java.util.Locale;
+import java.util.Scanner;
 
+public class main {
+    public static login user1 = new login("soroushes1" , "soroush" , "Soroush1382") ;
+    public static void main(String[] args) {
+        try {
+            usercheck();
+        }catch (LoginException e) {
+            e.printStackTrace();
+        }
+
+    }
+    public static void usercheck() throws LoginException{
+        Scanner Scan = new Scanner(System.in) ;
+
+        System.out.print("enter your name : ");
+        String name = Scan.nextLine() ;
+        name=name.toLowerCase(Locale.ROOT) ;
+
+        System.out.print("enter your username : ");
+        String username = Scan.nextLine() ;
+        username=username.toLowerCase(Locale.ROOT) ;
+
+        System.out.print("enter your password : ");
+        String pass = Scan.nextLine() ;
+
+        if (name.equals(user1.name) && username.equals(user1.username) && pass.equals(user1.pass)) {
+            System.out.println("loginned succesfully");
+        }
+        else {
+            throw new LoginException("name or username or password is incorrect ! ") ;
+        }
+    }
+}
