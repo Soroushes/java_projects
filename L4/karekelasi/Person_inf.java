3bc481f833541b940208ea7b7f778da63b5d6f53
Soroushes <99620495+Soroushes@users.noreply.github.com>
2022-03-23 18:25:09 +0430

    Add files via upload

---
+++ L4/karekelasi/Person_inf.java
@@ -0,0 +1,22 @@
+public class Person_inf {
+    private String Username , Pass , Job , Name , Family ;
+    private int Age ;
+    public void setUsername(String Username) {
+        this.Username = Username ;
+    }
+    public void setPass(String Pass) {
+        this.Pass = Pass ;
+    }
+    public void setJob(String Job) {
+        this.Job = Job ;
+    }
+    public void setName(String Name) {
+        this.Name = Name ;
+    }
+    public void setFamily(String Family) {
+        this.Family = Family ;
+    }
+    public void setAge(int Age) {
+        this.Age = Age ;
+    }
+}
--- L4/karekelasi/main.java
+++ L4/karekelasi/main.java
@@ -1,1 +1,11 @@
-
+public class main {
+    public static void main(String[] args) {
+        Person_inf p1 = new Person_inf() ;
+        p1.setAge(18);
+        p1.setName("soroush");
+        p1.setFamily("esmailian");
+        p1.setJob("Computer eng") ;
+        p1.setUsername("soroushes1");
+        p1.setPass("Soroush123es@") ;
+    }
+}
